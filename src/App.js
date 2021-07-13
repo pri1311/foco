62f977144bfc01e3b45595226a436b2a62b58d58
Priya Nagda <pri1311@users.noreply.github.com>
2021-07-13 23:59:56 +0530

    Add form to set pomodoro durations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import RandomQuote from "./components/random-quote";
+import SetPomodoro from "./components/set-pomodoro";
 
 function App() {
 	return (
 		<div className="container">
 			<h1>Pomodoro</h1>
 			<RandomQuote />
+			<SetPomodoro />
 		</div>
 	);
 }
--- src/components/set-pomodoro.js
+++ src/components/set-pomodoro.js
@@ -1,32 +1,71 @@
-import Button from "./button";
+import { useState } from "react";
 
-const setPomodoro = () => {
-	const handleChange = () => {};
+import Button from "./button";
+import styles from "./set-pomodoro.module.css";
+
+const SetPomodoro = () => {
+	const [timer, setTimer] = useState({
+		duration: 25,
+		shortBreak: 5,
+		longBreak: 15,
+		active: "duration",
+	});
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		switch (name) {
+			case "duration":
+				setTimer({
+					...timer,
+					duration: parseInt(value),
+				});
+				break;
+			case "shortBreak":
+				setTimer({
+					...timer,
+					shortBreak: parseInt(value),
+				});
+				break;
+			case "longBreak":
+				setTimer({
+					...timer,
+					longBreak: parseInt(value),
+				});
+				break;
+			default:
+				break;
+		}
+	};
+
+	const handleSubmit = () => {};
 
 	return (
-		<div className="form-container">
-			<form>
-				<div className="input-wrapper">
+		<div>
+			<form className={styles["form-container"]}>
+				<div className={styles["input-wrapper"]}>
 					<input
-						className="input"
+						className={styles.input}
 						name="duration"
 						onChange={handleChange}
+						defaultValue={timer.duration}
 					></input>
 					<input
-						className="input"
+						className={styles.input}
 						name="shortBreak"
 						onChange={handleChange}
+						defaultValue={timer.shortBreak}
 					></input>
 					<input
-						className="input"
+						className={styles.input}
 						name="longBreak"
 						onChange={handleChange}
+						defaultValue={timer.longBreak}
 					></input>
 				</div>
-				<Button title="Set Timer" />
+				<Button title="Set Timer" callback={handleSubmit} />
 			</form>
 		</div>
 	);
 };
 
-export default setPomodoro;
+export default SetPomodoro;
